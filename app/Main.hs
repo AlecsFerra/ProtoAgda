9ca0164d65876db8991897aee0bb9292a05280f9
AlecsFerra <alecsferra@protonmail.com>
2024-07-13 14:17:54 +0200

    Exposed better api to call the language

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,27 @@ module Main (main) where
 import Parsing.Parse (parse)
 import Language.Core.Engine (runProgram)
 import Language.Surface.Compiler (compile)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Language.Core.Name (MonadName, runNameT)
+import Language.Core.Name (runNameT)
+import System.Environment (getArgs)
 
-runM :: (MonadIO m, MonadName m) => String -> m ()
-runM path = do
-  src <- liftIO $ readFile path
+run :: String -> IO ()
+run src = do
   let parsed = parse src
   case parsed of
     Left error -> do 
-      liftIO $ print error
+      print error
     Right parsed -> do
-      compiled <- compile parsed
-      runProgram compiled
+      runNameT $ compile parsed >>= runProgram
 
-run :: String -> IO ()
-run path = runNameT $ runM path
+runFile :: String -> IO ()
+runFile path = do
+  src <- readFile path
+  run src
 
 main :: IO ()
 main = do
-  run "./test/exampleDiscard.magda"
+  args <- getArgs
+  case args of
+    [fileName] -> runFile fileName
+    _ -> print "Wrong number of arguments"
+  
